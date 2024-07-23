b7e8a50485a6d770638b84d40d0ebd339f27a6a6
kuangwanghangping <142001244+kuangwanghangping@users.noreply.github.com>
2024-07-23 21:14:30 +0800

    Add files via upload

--- miniqmt下载数据和读取数据/下载数据.py
+++ miniqmt下载数据和读取数据/下载数据.py
@@ -1,8 +1,21 @@
+import time
+
+from crazy import *
+dataframe_completely_display()
+pro = ts.pro_api('ad047441cc72120d3197505b58f84964148b449405d5e67b410a8bac')
+df = pro.cb_basic(fields="ts_code,bond_short_name,stk_code,stk_short_name,list_date,delist_date,remain_size")
+df = df[df['delist_date'].isna()]  # 删掉已经退市的转债
+df = df[df['remain_size'] != 0]  # 删掉剩余规模为0的转债
+df = df[~df['list_date'].isna()]  # 删掉还没有可以上市交易的转债
+dict1 = dict(zip(df['ts_code'],df['stk_code']))
+print(dict1)
 from xtquant import xtdata
-import pandas as pd
-def on_progress(data):
-	print(data)
-stock_list = ['123218.SZ','123106.SZ']
-start_time = '20240411092500'
-end_time = '20240411093100'
-xtdata.download_history_data2(stock_list=stock_list, period='1m', start_time=start_time, end_time=end_time, callback=on_progress)
+#获取可转债基础信息列表
+df = pro.cb_basic(fields="ts_code,bond_short_name,stk_code,stk_short_name,list_date,delist_date")
+for i ,ii in tqdm(dict1.items()):
+    code = ii
+    period = '1m'
+    day = '20240722'
+    day1 = '20240722'
+    xtdata.download_history_data(stock_code=code, period=period, start_time=day, end_time=day1)
+    time.sleep(0.1)
--- miniqmt下载数据和读取数据/读取数据.py
+++ miniqmt下载数据和读取数据/读取数据.py
@@ -1,28 +1,39 @@
+import time
+
 from xtquant import xtdata
-import pandas as pd
-def market_data_into_format(code_list, data):
-    """
-    转换xtquant库返回的数据格式
-    :return:dict
-    """
-    format_dict = {}
-    for code in code_list:
-        code_df = pd.DataFrame()
-        for column_name in data.keys():
-            column_df = data[column_name]
-            code_df[column_name] = column_df.loc[code]
-        format_dict[code] = code_df
-    return format_dict
-stock_list = ['123218.SZ','123106.SZ']
-start_time = '20240411092500'
-end_time = '20240411093100'
-data = xtdata.get_market_data(field_list=[],
-							  stock_list=stock_list,
-							  period='1m',
-							  start_time=start_time,
-							  end_time= end_time,
-							  count=1000,
-							  dividend_type='none',
-							  fill_data=True,)
-data_dict = market_data_into_format(stock_list,data)
-print(data_dict)
+from crazy import *
+dataframe_completely_display()
+pro = ts.pro_api('ad047441cc72120d3197505b58f84964148b449405d5e67b410a8bac')
+df = pro.cb_basic(fields="ts_code,bond_short_name,stk_code,stk_short_name,list_date,delist_date,remain_size")
+df = df[df['delist_date'].isna()]  # 删掉已经退市的转债
+df = df[df['remain_size'] != 0]  # 删掉剩余规模为0的转债
+df = df[~df['list_date'].isna()]  # 删掉还没有可以上市交易的转债
+dict1 = dict(zip(df['ts_code'],df['stk_code']))
+print(dict1)
+zhangfu_dict = {}
+chengjiaoe_dict = {}
+for i ,ii in tqdm(dict1.items()):
+    code =[f'{ii}']
+    period = '1m'
+    day = '20240722'
+    data_dir = r'G:\国金QMT交易端模拟\userdata_mini\datadir'
+    kline_data = xtdata.get_local_data(field_list=[], stock_list=code, period=period, start_time=day,
+                                       end_time=day, count=-1, data_dir=data_dir)
+    try:
+        #print(str(ii)+str((kline_data[f'{ii}']['open'].tolist()[1]-kline_data[f'{ii}']['open'].tolist()[0])/kline_data[f'{ii}']['open'].tolist()[0]*100))
+        #这个是每个股票开盘一刻的涨幅
+        zhangfu_dict[ii]=(kline_data[f'{ii}']['open'].tolist()[1]-kline_data[f'{ii}']['open'].tolist()[0])/kline_data[f'{ii}']['open'].tolist()[0]*100
+        chengjiaoe_dict[ii] = kline_data[f'{ii}']['amount'].tolist()[0]
+        time.sleep(0.1)
+    except:
+        pass
+print(chengjiaoe_dict)
+for i,ii in dict1.items():
+    try:
+        if chengjiaoe_dict[ii] < 700000:
+            del zhangfu_dict[ii]
+    except:
+        pass
+
+print(dict_rank(zhangfu_dict,'origin'))#这个是排序好的字典
+
